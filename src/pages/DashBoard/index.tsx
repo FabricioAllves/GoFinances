d06c9e2cb3ba3eb1ceba5c06448455442e717ac9
fabricio henrique alves <fabricioohh@gmail.com>
2023-02-02 21:48:20 -0300

    Update - adicionado paleta de cores para o app

---
+++ src/pages/DashBoard/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text } from "react-native";
+
+import { Container } from "./styles";
+
+export function DashBoard(){
+  return(
+    <Container>
+      <Text>DashBoard</Text>
+    </Container>
+  )
+}
+
