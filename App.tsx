025df8c874a1d8757c4952395b076a7e41959e7e
fabricio henrique alves <fabricioohh@gmail.com>
2023-02-02 23:06:58 -0300

    update

--- App.tsx
+++ App.tsx
@@ -1,11 +1,26 @@
 import React from "react";
 import { ThemeProvider } from 'styled-components'
 
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins'
+
 import theme from './src/global/styles/theme'
 import { DashBoard } from "./src/pages/DashBoard";
+import { Loading } from "./src/components/Loading";
 
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({Poppins_400Regular, Poppins_500Medium, Poppins_700Bold});
+
+  if(!fontsLoaded) {
+    return <Loading />
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <DashBoard />
