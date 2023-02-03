d3cb0a7561a4494035f27538a4832c86cfb2f13e
fabricio henrique alves <fabricioohh@gmail.com>
2023-02-02 22:11:47 -0300

    Update - typagem do thema

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { ThemeProvider } from 'styled-components'
+
+import theme from './src/global/styles/theme'
 import { DashBoard } from "./src/pages/DashBoard";
 
 
 export default function App() {
   return (
-   <DashBoard />
+    <ThemeProvider theme={theme}>
+      <DashBoard />
+    </ThemeProvider>
   );
 }
 
